4121f8ddfc037846bdef9574522adf05f494c319
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-03-11 06:13:17 +0000

    add resources

--- trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/Home.java
+++ trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/Home.java
@@ -1,25 +1,15 @@
 package com.josuvladimir.neuropsychologie;
 
-import android.app.ListActivity;
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ListView;
 
-public class Home extends ListActivity{
+public class Home extends Activity{
 	/** Called when the activity is first created. */
 	ListView mListView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
-		mListView = getListView();
     }
-    @Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		Intent intent = new Intent();
-		intent.setClass(this, SubList.class);
-		startActivity(intent);
-	}
 }
