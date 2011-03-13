a87db63d2c31a4485a1a446efb640b35cd308d12
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-03-13 23:07:31 +0000

    change home and list

--- trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/Home.java
+++ trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/Home.java
@@ -1,15 +1,31 @@
 package com.josuvladimir.neuropsychologie;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
 
-public class Home extends Activity{
+public class Home extends Activity implements OnClickListener{
 	/** Called when the activity is first created. */
 	ListView mListView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+		findViewById(R.id.home_button).setOnClickListener(this);
     }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.home_button:
+			Intent intent = new Intent(this, List.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+	}
 }
--- trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/List.java
+++ trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/List.java
@@ -1,10 +1,25 @@
 package com.josuvladimir.neuropsychologie;
 
 import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class List extends ListActivity {
-	@SuppressWarnings("unused")
 	private String[] mStrings;
-	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list);
+		mStrings = getResources().getStringArray(R.array.home_list);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, mStrings);
+		setListAdapter(adapter);
+	}
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+	}
 
 }
