eb12d18feceb1d10dc599b120979145c67e6bfbe
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-03-14 14:06:06 +0000

    add application class and data hash map

--- trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/Home.java
+++ trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/Home.java
@@ -5,11 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ListView;
 
 public class Home extends Activity implements OnClickListener{
 	/** Called when the activity is first created. */
-	ListView mListView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,12 +18,15 @@ public class Home extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.home_button:
-			Intent intent = new Intent(this, List.class);
-			startActivity(intent);
+			loadList();
 			break;
 
 		default:
 			break;
 		}
+	}
+	public void loadList(){
+		Intent intent = new Intent(this, List.class);
+		startActivity(intent);		
 	}
 }
--- trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/SubList.java
+++ trunk/Neuropsychologie/src/com/josuvladimir/neuropsychologie/SubList.java
@@ -1,22 +1,33 @@
 package com.josuvladimir.neuropsychologie;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class SubList extends ListActivity {
 
-	private String[] strings;
+//	private String[] mStrings;
+	private ArrayList<String> mStrings;
+	private TextView	mTextView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sublist);
-		strings = getResources().getStringArray(R.array.data_1_01);
+		mTextView = (TextView) findViewById(R.id.sublist_title);
+		mStrings = new ArrayList<String>();
+		Bundle bundle = getIntent().getExtras();
+		mTextView.setText(bundle.getString(Application.SUBLIST));
+		int id = bundle.getInt(Application.LIST_ID);
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,strings);
+		getData(id);
+//		mStrings = getResources().getStringArray(R.array.data_1_01);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mStrings);
 		setListAdapter(adapter);
 	}
 	@Override
@@ -32,5 +43,21 @@ public class SubList extends ListActivity {
 	}
 	private void showTextView(int id) {
 		
+	}
+	public void getData(int id) {
+		String string = "R.array.data_" + id + "_";
+		String tmpString;
+		String[] tmpStrings;
+		int	arrayID;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				tmpString = string + i + j;
+				if(Application.checkData(tmpString)){
+					arrayID = Application.getData(tmpString);
+					tmpStrings = getResources().getStringArray(arrayID);
+					mStrings.add(tmpStrings[0]);
+				}
+			}
+		}
 	}
 }
