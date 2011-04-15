14353a317f128d0d996f34e3088605f7a0ba813d
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-04-15 22:29:30 +0000

    add first mp3 file to play

--- trunk/Katzenshaker/src/com/josuvladimir/katzenshaker/Home.java
+++ trunk/Katzenshaker/src/com/josuvladimir/katzenshaker/Home.java
@@ -1,13 +1,25 @@
 package com.josuvladimir.katzenshaker;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Home extends Activity {
-    /** Called when the activity is first created. */
+public class Home extends Activity implements OnClickListener {
+	MediaPlayer mPlayer;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.home_button).setOnClickListener(this);
+        mPlayer = MediaPlayer.create(this, R.raw.i_love_u_baby);
     }
+	@Override
+	public void onClick(View v) {
+		play();
+	}
+	private void play() {
+		mPlayer.start();
+	}
 }
