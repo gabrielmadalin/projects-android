ecd85b7deec3f4da519f9aba6166a0f529d9ad89
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-04-15 23:28:57 +0000

    first shake functionality

--- Katzenshaker/src/com/josuvladimir/katzenshaker/Home.java
+++ Katzenshaker/src/com/josuvladimir/katzenshaker/Home.java
@@ -1,25 +1,66 @@
 package com.josuvladimir.katzenshaker;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class Home extends Activity implements OnClickListener {
-	MediaPlayer mPlayer;
+public class Home extends Activity implements OnClickListener, SensorEventListener {
+	private MediaPlayer mPlayer;
+	private SensorManager mSensorManager;
+	private float mLastAccel;
+	private float mCurrentAccel;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         findViewById(R.id.home_button).setOnClickListener(this);
         mPlayer = MediaPlayer.create(this, R.raw.i_love_u_baby);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mCurrentAccel = SensorManager.GRAVITY_EARTH;
     }
 	@Override
 	public void onClick(View v) {
 		play();
 	}
 	private void play() {
-		mPlayer.start();
+		try {
+			if (!mPlayer.isPlaying()) {
+				mPlayer.start();
+//				mPlayer.prepareAsync();
+			}
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+		mLastAccel = mCurrentAccel;
+		mCurrentAccel = (float) Math.sqrt((double)(x*x + y*y + z*z));
+		float delta = mCurrentAccel - mLastAccel;
+		if (delta > 15) {
+			play();
+		}
+	}
+	@Override
+	protected void onPause() {
+		mSensorManager.unregisterListener(this);
+		super.onPause();
+	}
+	@Override
+	protected void onResume() {
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+		super.onResume();
 	}
 }
