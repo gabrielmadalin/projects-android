23fc652b3649ea86e675b948c912e16047420c1c
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-04-16 16:48:46 +0000

    change shake detection using ShakeEventListener

--- Katzenshaker/src/com/josuvladimir/katzenshaker/Home.java
+++ Katzenshaker/src/com/josuvladimir/katzenshaker/Home.java
@@ -2,19 +2,20 @@ package com.josuvladimir.katzenshaker;
 
 import android.app.Activity;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class Home extends Activity implements OnClickListener, SensorEventListener {
+import com.josuvladimir.util.ShakeEventListener;
+import com.josuvladimir.util.ShakeEventListener.OnShakeListener;
+
+public class Home extends Activity implements OnClickListener, OnShakeListener {
 	private MediaPlayer mPlayer;
 	private SensorManager mSensorManager;
-	private float mLastAccel;
-	private float mCurrentAccel;
+	private ShakeEventListener mShakeEventListener;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +23,8 @@ public class Home extends Activity implements OnClickListener, SensorEventListen
         findViewById(R.id.home_button).setOnClickListener(this);
         mPlayer = MediaPlayer.create(this, R.raw.i_love_u_baby);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mCurrentAccel = SensorManager.GRAVITY_EARTH;
+        mShakeEventListener = new ShakeEventListener();
+        mShakeEventListener.setOnShakeListener(this);
     }
 	@Override
 	public void onClick(View v) {
@@ -31,36 +33,28 @@ public class Home extends Activity implements OnClickListener, SensorEventListen
 	private void play() {
 		try {
 			if (!mPlayer.isPlaying()) {
+				Toast.makeText(this, "Start playing", Toast.LENGTH_LONG).show();
 				mPlayer.start();
 //				mPlayer.prepareAsync();
+			} else {
+				Toast.makeText(this, "Already playing", Toast.LENGTH_LONG).show();
 			}
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		}
 	}
 	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	}
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		float x = event.values[0];
-		float y = event.values[1];
-		float z = event.values[2];
-		mLastAccel = mCurrentAccel;
-		mCurrentAccel = (float) Math.sqrt((double)(x*x + y*y + z*z));
-		float delta = mCurrentAccel - mLastAccel;
-		if (delta > 15) {
-			play();
-		}
-	}
-	@Override
 	protected void onPause() {
-		mSensorManager.unregisterListener(this);
+		mSensorManager.unregisterListener(mShakeEventListener);
 		super.onPause();
 	}
 	@Override
 	protected void onResume() {
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(mShakeEventListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
 		super.onResume();
+	}
+	@Override
+	public void onShake() {
+		play();
 	}
 }
