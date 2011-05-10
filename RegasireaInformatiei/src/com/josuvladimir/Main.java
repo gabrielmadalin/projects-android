19b1c4ebfba1ad6dcb44bedac6e26ca3161288c6
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-05-10 22:33:24 +0000

    add output file

--- RegasireaInformatiei/src/com/josuvladimir/Main.java
+++ RegasireaInformatiei/src/com/josuvladimir/Main.java
@@ -3,11 +3,10 @@ package com.josuvladimir;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
@@ -18,39 +17,35 @@ import com.josuvladimir.util.Util;
 
 public class Main {
 
-	private static final String FILE_PATH = "D:/Documents/WORK/Projects/RegasireaInformatiei/res/input.txt";
+	public static final String INPUT_FILE_PATH = "D:/Documents/WORK/Projects/RegasireaInformatiei/res/input.txt";
+	public static final String OUTPUT_FILE_PATH = "D:/Documents/WORK/Projects/RegasireaInformatiei/res/output.txt";
+	private static File mOutputFile;
 
 	public static void main(String[] args) {
-		Init();
+		try {
+			init();
+			process();
+		} catch (IOException e) {
+			Util.log("Error:" + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	@SuppressWarnings("unused")
-	private static void Init() {
-		File file = new File(FILE_PATH);
+	private static void init() throws IOException {
+		File file = new File(INPUT_FILE_PATH);
 		FileInputStream inputStream;
 		Util.log("test");
-		try {
-			inputStream = new FileInputStream(file);
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		inputStream = new FileInputStream(file);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+		mOutputFile = new File(OUTPUT_FILE_PATH);
+		new FileWriter(mOutputFile);
 	}
 
 	@SuppressWarnings({ "unused", "deprecation" })
-	private static void process() {
-		try {
-			Directory directory = FSDirectory.open(new File(FILE_PATH));
-			IndexSearcher searcher = new IndexSearcher(directory);
-			QueryParser parser = new QueryParser(Version.LUCENE_29, "contents", new RoAnalyzer(Version.LUCENE_29));
-			
-		} catch (CorruptIndexException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	private static void process() throws IOException {
+		Directory directory = FSDirectory.open(new File(INPUT_FILE_PATH));
+		IndexSearcher searcher = new IndexSearcher(directory);
+		QueryParser parser = new QueryParser(Version.LUCENE_29, "contents", new RoAnalyzer(Version.LUCENE_29));
 	}
 }
