98abfd9e9b61b0d07080ce5d8099cbe4e9468934
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-10-09 20:22:15 +0000

    added text validator

--- trunk/Utils/src/com/josuvladimir/util/UtilsActivity.java
+++ trunk/Utils/src/com/josuvladimir/util/UtilsActivity.java
@@ -1,10 +1,13 @@
 package com.josuvladimir.util;
 
+import com.josuvladimir.util.EditTextValidator.Type;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class UtilsActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
@@ -13,6 +16,8 @@ public class UtilsActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         findViewById(R.id.main_btn_carousel).setOnClickListener(this);
+        new EditTextValidator((EditText) findViewById(R.id.main_edit_text), Type.MAIL);
+        findViewById(R.id.main_edit_text);
     }
 
 	@Override
