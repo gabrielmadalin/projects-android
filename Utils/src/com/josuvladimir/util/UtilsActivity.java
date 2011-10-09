f54e90486821ab8a597628e140a70fe6fa6ebf9d
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-10-09 20:17:59 +0000

    added carousel

--- Utils/src/com/josuvladimir/util/UtilsActivity.java
+++ Utils/src/com/josuvladimir/util/UtilsActivity.java
@@ -1,13 +1,30 @@
 package com.josuvladimir.util;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class UtilsActivity extends Activity {
+public class UtilsActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.main_btn_carousel).setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.main_btn_carousel:
+			Intent intent = new Intent(this, CarouselActivity.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+	}
 }
