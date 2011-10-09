6543d1011ed5d8c731a99a1fb95ffd1f29f1c83c
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-10-09 21:33:45 +0000

    added shake event listener

--- Utils/src/com/josuvladimir/util/UtilsActivity.java
+++ Utils/src/com/josuvladimir/util/UtilsActivity.java
@@ -5,21 +5,36 @@ import com.josuvladimir.util.ShakeEventListener.OnShakeListener;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 
 public class UtilsActivity extends Activity implements OnClickListener, OnShakeListener {
-    /** Called when the activity is first created. */
+    private MediaPlayer mPlayer;
+	private ShakeEventListener mShakeEventListener;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         findViewById(R.id.main_btn_carousel).setOnClickListener(this);
         new EditTextValidator((EditText) findViewById(R.id.main_edit_text), Type.MAIL);
-        new ShakeEventListener(this,this);
+        mShakeEventListener = new ShakeEventListener(this,this);
+		mPlayer = MediaPlayer.create(this, R.raw.i_love_u_baby);
+//		mPlayer.prepareAsync();
     }
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mShakeEventListener.unregister();
+	}
 
 	@Override
 	public void onClick(View view) {
@@ -35,6 +50,9 @@ public class UtilsActivity extends Activity implements OnClickListener, OnShakeL
 
 	@Override
 	public void onShake() {
+		if (!mPlayer.isPlaying()) {
+			mPlayer.start();
+		}
 		loadCarousel();
 	}
 
