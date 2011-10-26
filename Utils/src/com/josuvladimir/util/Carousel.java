4efc0c84d4874a4d139523883361d0038a5211a2
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-10-26 08:36:15 +0000

    changed to library, removed activities

--- Utils/src/com/josuvladimir/util/Carousel.java
+++ Utils/src/com/josuvladimir/util/Carousel.java
@@ -1,13 +1,23 @@
 package com.josuvladimir.util;
 
 import android.content.Context;
+import android.graphics.Camera;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Transformation;
 import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class Carousel extends Gallery {
 
+	private int minZoom = 500;
+	private int maxZoom = 0;
+	private Camera mCamera = new Camera();
+	private int mCarouselCenter;
+	private int mWidth;
+	private int mImageViewId;
+	
 	public Carousel(Context context) {
 		super(context);
 		setStaticTransformationsEnabled(true);
@@ -23,26 +33,63 @@ public class Carousel extends Gallery {
 		setStaticTransformationsEnabled(true);
 	}
 
-	@SuppressWarnings("unused")
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		mCarouselCenter = getViewCenter(this);
+		mWidth = getLeft() + getWidth();
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+
 	@Override
 	protected boolean getChildStaticTransformation(View child, Transformation transformation) {
+		ImageView imageView = (ImageView) child.findViewById(mImageViewId);
 		int viewCenter = getViewCenter(child);
-		int width = child.getWidth();
-		getCarouselCenter();
 		transformation.clear();
 		transformation.setTransformationType(Transformation.TYPE_MATRIX);
-		
-		
-//		return super.getChildStaticTransformation(child, t);
+		float zoomPercent = (Math.abs(viewCenter - mCarouselCenter)) / (float) mWidth;
+		float currentZoom = maxZoom - (maxZoom - minZoom) * zoomPercent;
+		transformView(imageView, transformation, currentZoom);
 		return true;
 	}
 
-	private int getCarouselCenter() {
-		return (getWidth() - getPaddingLeft() - getPaddingRight()) / 2 + getPaddingLeft();
+	private void transformView(ImageView view, Transformation transformation, float currentZoom) {
+		mCamera.save();
+		Matrix matrix = transformation.getMatrix();
+		int width = view.getWidth();
+		int height = view.getHeight();
+		mCamera.translate(0, 0, currentZoom);
+		mCamera.getMatrix(matrix);
+		matrix.preTranslate(-width/2, -height/2);
+		matrix.postTranslate(width/2, height/2);
+		mCamera.restore();
 	}
 
 	private int getViewCenter(View view) {
 		int center = view.getLeft() + view.getWidth() / 2;
 		return center;
+	}
+
+	public void setMinZoom(int minZoom) {
+		this.minZoom = minZoom;
+	}
+
+	public int getMinZoom() {
+		return minZoom;
+	}
+
+	public void setMaxZoom(int maxZoom) {
+		this.maxZoom = maxZoom;
+	}
+
+	public int getMaxZoom() {
+		return maxZoom;
+	}
+
+	public int getImageViewId() {
+		return mImageViewId;
+	}
+
+	public void setImageViewId(int mImageViewId) {
+		this.mImageViewId = mImageViewId;
 	}
 }
