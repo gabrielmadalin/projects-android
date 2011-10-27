10ce0984c6e0a9f5d5230b8e654014225ffb0e49
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-10-27 15:02:04 +0000

    sent sms

--- SmSender/src/com/josuvladimir/smsender/SmSenderActivity.java
+++ SmSender/src/com/josuvladimir/smsender/SmSenderActivity.java
@@ -1,14 +1,11 @@
 package com.josuvladimir.smsender;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.widget.Toast;
 
+@SuppressWarnings("unused")
 public class SmSenderActivity extends Activity {
-	private static final String scAddress = null;
 	private static String destinationAddress = "+40742503976";
 	private static final String ACTION_SMS_SENT = "com.josuvladimir.smsender.SMS_SENT_ACTION";
 
@@ -17,11 +14,8 @@ public class SmSenderActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        SmsManager smsManager = SmsManager.getDefault();
-        
-        String text = "test";
-    	destinationAddress = "+40744931398";
-		smsManager.sendTextMessage(destinationAddress, scAddress, text, PendingIntent.getBroadcast(this, 0, new Intent(ACTION_SMS_SENT), 0), null);
-		Toast.makeText(this, "Sended", Toast.LENGTH_SHORT).show();
+        String textMessage = "ce faci mere";
+		startService(new Intent(this,SmSenderService.class));
+		finish();
     }
 }
--- SmSender/src/com/josuvladimir/smsender/SmSenderService.java
+++ SmSender/src/com/josuvladimir/smsender/SmSenderService.java
@@ -1,15 +1,55 @@
 package com.josuvladimir.smsender;
 
+import com.josuvladimir.util.Util;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class SmSenderService extends Service {
 
+	private static final String ACTION_SMS_SENT = "com.josuvladimir.util.";
+	private LocationManager mLocationManager;
+	@SuppressWarnings("unused")
+	private LocationListener listener;
+
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
+		
 		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Toast.makeText(this, "SMService started", Toast.LENGTH_SHORT).show();
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		setLocationManager(mLocationManager);
+	}
+
+	private void setLocationManager(LocationManager locationManager) {
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+		String provider = locationManager.getBestProvider(criteria, true);
+//		locationManager.requestLocationUpdates(provider, MIN_TIME, MIN_DISTANCE, listener);
+		Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+		String destinationAddress = "+40742503976";
+		String textMessage = "Latitude: " + lastKnownLocation.getLatitude() + " Longitude: " + lastKnownLocation.getLongitude();
+		Util.sendSMS(this, destinationAddress, textMessage, ACTION_SMS_SENT);
+		
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "SMService destroyed", Toast.LENGTH_SHORT).show();
+		startService(new Intent(this,SmSenderService.class));
 	}
 
 }
