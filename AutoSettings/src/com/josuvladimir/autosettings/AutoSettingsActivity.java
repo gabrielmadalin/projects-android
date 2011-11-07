7675307110664c7eabbf021f7dd17b232aa4f040
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-11-07 19:13:48 +0000

    Initial import.

---
+++ AutoSettings/src/com/josuvladimir/autosettings/AutoSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.josuvladimir.autosettings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AutoSettingsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
