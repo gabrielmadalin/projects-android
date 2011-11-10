aff30f5f3ef8bfb3d92c8d98582a7192e85db0ca
josuvladimir@gmail.com <josuvladimir@gmail.com@5a495318-2376-bba2-fc22-e876bd3b797e>
2011-11-10 07:30:15 +0000

    added preference screen

--- trunk/AutoSettings/src/com/josuvladimir/autosettings/AutoSettingsActivity.java
+++ trunk/AutoSettings/src/com/josuvladimir/autosettings/AutoSettingsActivity.java
@@ -1,29 +1,44 @@
 package com.josuvladimir.autosettings;
 
-import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 
-public class AutoSettingsActivity extends Activity implements OnCheckedChangeListener {
-    private TextView mTextView;
+public class AutoSettingsActivity extends PreferenceActivity implements OnPreferenceChangeListener {
+    private static final String BOX_KEY = "box_vibrate";
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
-        toggleButton.setOnCheckedChangeListener(this);
-        mTextView = (TextView) findViewById(R.id.textView);
+//        setContentView(R.layout.main);
+//        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+//        toggleButton.setOnCheckedChangeListener(this);
+//        mTextView = (TextView) findViewById(R.id.textView);
+        setPreferenceScreen(createPreferenceHierarchy());
     }
 
-	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+	private PreferenceScreen createPreferenceHierarchy() {
+		PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+		PreferenceCategory category = new PreferenceCategory(this);
+		category.setTitle("Title");
+		root.addPreference(category);
+		CheckBoxPreference boxPreference = new CheckBoxPreference(this);
+		boxPreference.setKey(BOX_KEY);
+		boxPreference.setTitle("Vibrate");
+		boxPreference.setSummary("Summary");
+		boxPreference.setOnPreferenceChangeListener(this);
+		root.addPreference(boxPreference);
+		return root;
+	}
+
+	public void onCheckedChanged(boolean isChecked) {
 		AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 		int setting;
 		int ringerMode;
@@ -40,13 +55,22 @@ public class AutoSettingsActivity extends Activity implements OnCheckedChangeLis
 		int vibrateSetting = audioManager.getVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER);
 		switch (vibrateSetting) {
 		case AudioManager.VIBRATE_SETTING_ON:
-			mTextView.setText("Vibrate settings is on");
 			break;
 		case AudioManager.VIBRATE_SETTING_OFF:
-			mTextView.setText("Vibrate settings is off");
 			break;
 		default:
 			break;
 		}
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		String key = preference.getKey();
+		if (key.equalsIgnoreCase(BOX_KEY)) {
+			boolean b = (Boolean) newValue;
+			onCheckedChanged(b);
+			return true;
+		}
+		return false;
 	}
 }
